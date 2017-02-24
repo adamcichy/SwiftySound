3134ddd72e015ce6d11bf1d1b1bbcd189e6b82a6
Adam Cichy <adam@moonlightapps.com>
2017-02-24 12:50:00 +0100

    Added Xcode project with frameworks for iOS, macOS and tvOS.

---
+++ SwiftySound macOS/SwiftySound macOS.h
@@ -0,0 +1,19 @@
+//
+//  SwiftySound macOS.h
+//  SwiftySound macOS
+//
+//  Created by Adam Cichy on 24/02/17.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftySound macOS.
+FOUNDATION_EXPORT double SwiftySound_macOSVersionNumber;
+
+//! Project version string for SwiftySound macOS.
+FOUNDATION_EXPORT const unsigned char SwiftySound_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftySound_macOS/PublicHeader.h>
+
+
