cf5be0cd046404c917c634b029007411b92a5a7a
Chris Stamper <chris@cdstamper.co>
2019-06-14 16:23:48 -0400

    XCode 11 Support
    Swift Package Manager now requires declaring a swift-tools version, as well as declaring a product.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,10 @@
+    
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
     name: "SwiftySound"
+    products: [
+        .library(name: "SwiftySound", targets: ["SwiftySound"])
+    ],
 )
